c7858d5e9377cd774fa1fc1f1ba9a79fbb01a71a
Arthur-liger <arthur.liger@supdevinci-edu.fr>
2022-12-15 09:56:04 +0100

    creation des applications

--- Django_Project/Django_Project/settings.py
+++ Django_Project/Django_Project/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog'
 ]
 
 MIDDLEWARE = [
@@ -62,7 +61,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages'
             ],
         },
     },
--- Django_Project/create_account/apps.py
+++ Django_Project/create_account/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CatalogConfig(AppConfig):
+class CreateAccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'catalog'
+    name = 'create_account'
